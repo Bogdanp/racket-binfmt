6b35b2719639f629e2db66acd0c3c5473f987bdf
Bogdan Popa <bogdan@defn.io>
2022-01-20 08:29:43 +0200

    test: drop endianness from id3v1 test

--- binfmt-test/binfmt/test-id3.rkt
+++ binfmt-test/binfmt/test-id3.rkt
@@ -1,7 +1,6 @@
 #lang racket/base
 
-(require binfmt/runtime
-         rackunit
+(require rackunit
          "common.rkt"
          "id3v1.b")
 
@@ -13,8 +12,7 @@
     (lambda (in)
       (file-position in eof)
       (file-position in (- (file-position in) 128))
-      (parameterize ([current-endianness 'big])
-        (id3 in)))))
+      (id3 in))))
 
 (check-equal? (ref-bytes 'title_1 t)   #"Creative Commons Song         ")
 (check-equal? (ref-bytes 'artist_1 t)  #"Improbulus                    ")
