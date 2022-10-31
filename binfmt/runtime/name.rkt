016de3a3dcca991654afe5f3056458eb4e2da55e
Bogdan Popa <bogdan@defn.io>
2022-10-31 21:09:42 +0200

    runtime,name: improve the performance of next-name

--- binfmt/runtime/name.rkt
+++ binfmt/runtime/name.rkt
@@ -1,12 +1,37 @@
 #lang racket/base
 
+(#%declare #:unsafe)
+
+(require (for-syntax racket/base
+                     syntax/parse)
+         racket/symbol)
+
 (provide
- (all-defined-out))
+ current-name-seqs
+ next-name)
 
 (define current-name-seqs
   (make-parameter (make-hasheq)))
 
-(define (next-name id)
-  (define seqs (current-name-seqs))
-  (hash-update! seqs id add1 0)
-  (string->symbol (format "~a_~a" id (hash-ref seqs id))))
+(define (next-name id [seqs (current-name-seqs)])
+  (define seq (add1 (hash-ref seqs id 0)))
+  (hash-set! seqs id seq)
+  (string->symbol
+   (string-append
+    (symbol->immutable-string id)
+    (number->suffix seq))))
+
+(define-syntax (define-number->suffix-proc stx)
+  (define N 5)
+  (syntax-parse stx
+    [(_ id)
+     #:with (fast-path ...) (for/list ([i (in-range N)])
+                              (datum->syntax stx i))
+     #:with (fast-path-res ...) (for/list ([i (in-range N)])
+                                  (datum->syntax stx (format "_~a" i)))
+     #'(define (id n)
+         (case n
+           [(fast-path) fast-path-res] ...
+           [else (string-append "_" (number->string n))]))]))
+
+(define-number->suffix-proc number->suffix)
