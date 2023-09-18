f6d239f266ac9b3f56c69cba01a86a65b3c1865b
Bogdan Popa <bogdan@defn.io>
2023-09-18 20:24:31 +0300

    build: extract binfmt-lib pkg

---
+++ binfmt-lib/info.rkt
@@ -0,0 +1,6 @@
+#lang info
+
+(define license 'BSD-3-Clause)
+(define version "0.5")
+(define collection "binfmt")
+(define deps '("base"))
--- binfmt/info.rkt
+++ binfmt/info.rkt
@@ -1,8 +1,9 @@
 #lang info
 
 (define license 'BSD-3-Clause)
-(define version "0.4")
 (define collection "binfmt")
-(define deps '("base"))
+(define deps '("base"
+               "binfmt-lib"))
 (define build-deps '("racket-doc" "scribble-lib"))
 (define scribblings '(("binfmt-manual.scrbl" () (parsing-library))))
+(define implies '("binfmt-lib"))
